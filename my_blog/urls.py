334ef680eb03628b0b59e394017fbad7f82368d1
Maureen Ononiwu <59079323+Chinwendu20@users.noreply.github.com>
2021-04-29 11:58:36 +0100

    Updated urls.py to correct error

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('reset/done/',
         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
         name='password_reset_done'),
-    re_path('reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
+    re_path('reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,50})/',
         auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
         name='password_reset_confirm'),
     path('reset/complete/',
